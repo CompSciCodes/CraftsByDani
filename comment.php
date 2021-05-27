38beb6571cd5d000365f33f51046a7c9ef4ed37f
CompSciCodes <61157042+CompSciCodes@users.noreply.github.com>
2021-05-27 16:53:01 -0500

    Create comment.php

---
+++ comment.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Your product category of inquiry is: <?php echo $_POST["product"]; ?>
+Question or Comment: <?php echo $_POST["comment"]; ?>
+  
+
+</body>
+</html>
